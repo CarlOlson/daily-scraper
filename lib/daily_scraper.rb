3326b955409cd1447421f83040294ee3c43e812f
CarlOlson <CarlOlson@users.noreply.github.com>
2017-09-25 23:22:41 -0500

    eval blocks for configuration

--- lib/daily_scraper.rb
+++ lib/daily_scraper.rb
@@ -2,14 +2,19 @@ require "daily_scraper/version"
 
 module DailyScraper
   class Task
-    attr_reader :url
+    attr_reader :url, :plugins
 
     def initialize url
       @url = url
+      @plugins = []
+    end
+
+    def plugin plugin
+      @plugins << plugin
     end
   end
 
   def self.goto url
-    Task.new url
+    Task.new(url).tap { |t| yield t if block_given? }
   end
 end
--- spec/daily_scraper_spec.rb
+++ spec/daily_scraper_spec.rb
@@ -7,6 +7,7 @@ RSpec.describe DailyScraper do
 
   describe "goto" do
     let(:a_url) { "http://example.com" }
+    let(:canary) { spy "canary" }
 
     it "should return a task object" do
       expect(DailyScraper.goto a_url).not_to be nil
@@ -15,6 +16,20 @@ RSpec.describe DailyScraper do
     it "should accept a url" do
       task = DailyScraper.goto a_url
       expect(task.url).to eq a_url
+    end
+
+    it "should evaluate given blocks" do
+      task = DailyScraper.goto(a_url) do
+        canary.called
+      end
+      expect(canary).to have_received(:called).once
+    end
+
+    it "should register given plugins" do
+      task = DailyScraper.goto(a_url) do |t|
+        t.plugin canary
+      end
+      expect(task.plugins).to eq [canary]
     end
   end
 end
