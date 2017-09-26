3e757be17d1c3ad08fa6edbe3df768a4e32cb6f9
CarlOlson <CarlOlson@users.noreply.github.com>
2017-09-25 22:13:23 -0500

    init commit

---
+++ lib/daily/scraper.rb
@@ -0,0 +1,7 @@
+require "daily/scraper/version"
+
+module Daily
+  module Scraper
+    # Your code goes here...
+  end
+end
---
+++ lib/daily/scraper/version.rb
@@ -0,0 +1,5 @@
+module Daily
+  module Scraper
+    VERSION = "0.1.0"
+  end
+end
